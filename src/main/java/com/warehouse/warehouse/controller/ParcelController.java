efd36548d2d0976be951044ec62eb72869334b3c
Sebastian <sebastian5253@wp.pl>
2022-05-08 03:48:07 +0200

    VERSION_2022.4.1 Heroku deploy

--- src/main/java/com/warehouse/warehouse/controller/ParcelController.java
+++ src/main/java/com/warehouse/warehouse/controller/ParcelController.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 @RequestMapping("/api/parcels")
 @AllArgsConstructor
 public class ParcelController {
-
+//test
     private final ParcelService parcelService;
 
     @PostMapping
