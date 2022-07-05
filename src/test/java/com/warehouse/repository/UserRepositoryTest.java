83eb0a7e6238a0b0ab9e6551a89d1e413b1ef269
Sebastian Soja <sebastian5253@wp.pl>
2022-07-05 22:52:12 +0200

    [COUR-9107] Add user, supplier repository tests
    [INP-52]

--- src/test/java/com/warehouse/repository/UserRepositoryTest.java
+++ src/test/java/com/warehouse/repository/UserRepositoryTest.java
@@ -1,7 +1,58 @@
 package com.warehouse.repository;
 
+import com.warehouse.entity.Depot;
+import com.warehouse.entity.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private DepotRepository depotRepository;
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void shouldSaveUser() {
+        // given
+        final Depot depot = Depot.builder()
+                .city("Test")
+                .street("Test street")
+                .depotCode("TS1")
+                .country("Test")
+                .build();
+
+        // save in db
+        depotRepository.save(depot);
+
+
+        final User user = User.builder()
+                .username("test")
+                .password("test")
+                .email("test@test.test")
+                .lastName("test")
+                .firstName("test")
+                .depot(depot)
+                .role("admin")
+                .build();
+
+        // when
+        userRepository.save(user);
+
+        // then
+        assertThat(user).isNotNull();
+        assertThat(user.getId()).isEqualTo(1);
+
+    }
 }
