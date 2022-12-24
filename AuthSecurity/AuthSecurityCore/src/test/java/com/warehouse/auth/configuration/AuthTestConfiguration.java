52d1a34536c6aed92304c27f9f2203f4c5056897
Sebastian Soja <sebastian5253@wp.pl>
2022-12-24 17:34:40 +0530

    Add missing MockBean and disable controller tests

--- AuthSecurity/AuthSecurityCore/src/test/java/com/warehouse/auth/configuration/AuthTestConfiguration.java
+++ AuthSecurity/AuthSecurityCore/src/test/java/com/warehouse/auth/configuration/AuthTestConfiguration.java
@@ -1,6 +1,8 @@
 package com.warehouse.auth.configuration;
 
+import com.warehouse.depot.api.DepotService;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,5 +11,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EntityScan(basePackages = { "com.warehouse.auth" })
 @EnableJpaRepositories(basePackages = { "com.warehouse.auth" })
 public class AuthTestConfiguration {
+
+    @MockBean
+    public DepotService depotService;
 
 }
